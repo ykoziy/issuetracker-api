bbb55960a16670382f264ecde32205f127a93710
Yuriy Koziy <yukoziy@gmail.com>
2019-11-20 18:41:04 -0500

    Implemented POST route, it is passing tests.

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,7 @@ var expect = require('chai').expect;
 //var ObjectId = require('mongodb').ObjectID;
 
 const Issue = require('../models/Issue');
+const requiredFields = ['Title','Text','Created by'];
 
 module.exports = function (app) {
 
@@ -23,7 +24,23 @@ module.exports = function (app) {
     })
 
     .post(function (req, res){
-      var project = req.params.project;
+      // optional can be empty
+      let requiredFields = ['issue_title','issue_text','created_by'];
+      let areRequiredFieldsMissing = [!req.body.issue_title, !req.body.issue_text, !req.body.created_by];
+      let missingFields = requiredFields.filter((item, index) => {
+        if (areRequiredFieldsMissing[index]) {
+          return true;
+        }
+      });
+      if (missingFields.length > 0) {
+        return res.status(422).json({ error: `Missing fields: ${missingFields}`});
+      }
+
+      let newIssue = new Issue(req.body);
+      newIssue.save((err, data) => {
+        if (err) return console.error(err);
+        res.json(data);
+      });
 
     })
 
