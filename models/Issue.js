06d1b3eb087f0c044af6f639fc0c68e8eca2ab85
Yuriy Koziy <yukoziy@gmail.com>
2019-11-21 17:28:19 -0500

    Optional fields assigned_to and status_text now default to empty string.

--- models/Issue.js
+++ models/Issue.js
@@ -5,8 +5,8 @@ let IssueSchema = new Schema({
   issue_title: {type: String, required: true},
   issue_text: {type: String, required: true},
   created_by: {type: String, required: true},
-  assigned_to: String,
-  status_text: String,
+  assigned_to: {type: String, default: ''},
+  status_text: {type: String, default: ''},
   created_on: {type: Date, default: Date.now},
   updated_on: {type: Date, default: Date.now},
   open: {type: Boolean, default: true}
